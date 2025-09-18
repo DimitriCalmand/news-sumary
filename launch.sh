d3b9425987b9255550909976aed80c6ed5dc4ade
dipezed <dcalmand73@gmail.com>
2025-09-18 11:27:50 +0200

    feat: devops

--- launch.sh
+++ launch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -p 80:80 --name news-dev -e PORT=80 -e DEBUG=True -v $(pwd)/src:/app/src news-sumary
+docker run -it -p 80:80 --name news-dev -e PORT=80 -e DEBUG=True -v ./src:/app/src news-sumary
